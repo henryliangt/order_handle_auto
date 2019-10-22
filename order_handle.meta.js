a5372fe2e5bfa256fee324100e39a8bcf86bfb64
A380 <henryliangt@gmail.com>
2019-10-23 00:17:48 +0800

    Create order_handle.meta.js
    1.0 user script meta

---
+++ order_handle.meta.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Tokopedia order
+// @namespace    http://www.tiaria.id/
+// @version      0.1
+// @description  Handle tokopedia orders
+// @author       Henry
+// @connect      www.tiaria.id
+// @run-at       document-end
+// @require      http://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js
+// @require      https://cdn.jsdelivr.net/jsbarcode/3.6.0/JsBarcode.all.min.js
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @match        https://*.tokopedia.com/*
+// @updateURL    https://raw.githubusercontent.com/henryliangt/order_handle_auto/master/order_handle.meta.js
+// @downloadURL  https://raw.githubusercontent.com/henryliangt/order_handle_auto/master/order_handle.user.js
