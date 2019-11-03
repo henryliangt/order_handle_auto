19986d435ff08a6a5f21e2f1543aa8650f86ec29
A380 <henryliangt@gmail.com>
2019-11-04 01:19:17 +0800

    Update order_handle.user.js

--- order_handle.user.js
+++ order_handle.user.js
@@ -1,15 +1,23 @@
 // ==UserScript==
-// @name         Tokopedia Bukalapak order barcode
+// @name         Tokopedia Bukalapak order barcode &tkpd order update.
 // @namespace    http://www.tiaria.id/
-// @version      0.13
-// @description  Handle tokopedia orders
-// @author       Henry
+// @version      0.14
+// @description  Updating tkpd order, insert bukalapak  tkpd order number barcode.
+// @author       HL
 // @connect      www.tiaria.id
+// @connect      google.com
+// @connect      https://script.google.com/
+// @connect      https://googleusercontent.com/
+// @connect      https://script.googleusercontent.com/
+// @connect      script.googleusercontent.com
+// @connect      *
 // @run-at       document-end
 // @require      http://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js
 // @require      https://cdn.jsdelivr.net/jsbarcode/3.6.0/JsBarcode.all.min.js
 // @grant        GM_setValue
 // @grant        GM_getValue
+// @grant        GM_log
+// @grant        GM_xmlhttpRequest
 // @match        https://*.tokopedia.com/*
 // @match        https://www.bukalapak.com/payment/transactions/print_preview*
 // @updateURL    https://raw.githubusercontent.com/henryliangt/order_handle_auto/master/order_handle.meta.js
