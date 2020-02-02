a78fbd39c3e8ec729cb2171e864bf11484f71929
henryliangt <henryliangt@gmail.com>
2020-02-03 03:43:48 +0800

    add the feature to verify order in database or not. first testing

--- tkpd-status.user.js
+++ tkpd-status.user.js
@@ -59,21 +59,27 @@
 						sku_elem[i].textContent = order;
 						sku_elem[i].style       = "font-size:150%; color:black; font-family:verdana;"
 					};
-					var invoice_urls_arr = collect_invoice();
-					console.log('ALL invoice=  ' + typeof invoice_urls_arr + "   length=" + invoice_urls_arr.length + invoice_urls_arr + '=' );
+					var invoice_urls_arr    = [];
+					var invoice_urls_elem   = document.querySelectorAll('a.invoice');
+					for(var k=0; k<invoice_urls_elem.length; k++){
+					console.log(invoice_urls_elem[k].href);
+					invoice_urls_arr.push(invoice_urls_elem[k].href);
+					console.log('in 1st func');
+					};
 				},
 			});
-
+			var invoice_urls_arr = collect_invoice();
+			console.log('ALL invoice=  ' + typeof invoice_urls_arr + "   length=" + invoice_urls_arr.length + invoice_urls_arr + '=' );
 			function collect_invoice (){
 			var invoice_urls_arr    = [];
 			var invoice_urls_elem   = document.querySelectorAll('a.invoice');
-
-			// var invoice_urls_arr    = ['https://www.tokopedia.com/invoicessssssss'];
-			var invoice_urls_tosend = [];
-			for(var k=0; k<invoice_urls_elem.length; k++){
+			for(var k=0; k<invoice_urls_elem.length; k++) {
 				console.log(invoice_urls_elem[k].href);
 				invoice_urls_arr.push(invoice_urls_elem[k].href);
 			}
+			// var invoice_urls_arr    = ['https://www.tokopedia.com/invoicessssssss'];
+			var invoice_urls_tosend = [];
+
 			console.log('ALL invoice=  ' + typeof invoice_urls_arr + "   length=" + invoice_urls_arr.length + invoice_urls_arr + '=' );
 			var index               = 0;
 			var invoice_amount      = invoice_urls_arr.length;
